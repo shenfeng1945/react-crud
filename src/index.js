f219bb62a8a9c293e15a10f99c8c6071f998fd35
shenfeng1945 <823902826@qq.com>
2018-08-25 22:50:36 +0800

    设置router和semantic-ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GamesPage from './components/GamesPage'
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux'
-import {createStore,applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 const store = createStore(
     rootReducer,
@@ -16,8 +18,18 @@ const store = createStore(
 )
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
- document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <div className="ui container">
+                <div className="ui three item menu">
+                    <NavLink exact activeClassName="active" to="/" className="item">Home</NavLink>
+                    <NavLink exact activeClassName="active" to="/game" className="item">Game</NavLink>
+                    <NavLink exact activeClassName="active" to="/game/new" className="item">New Game</NavLink>
+                </div>
+                <Route exact path="/" component={App} />
+                <Route exact path="/game" component={GamesPage} />
+            </div>
+        </Router>
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();

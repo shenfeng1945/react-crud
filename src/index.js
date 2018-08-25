0c5c1c4b6c91834446d723d8b83f3a7070d12642
shenfeng1945 <823902826@qq.com>
2018-08-25 22:16:02 +0800

    配置redux和中间件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware} from 'redux'
+import rootReducer from './reducers'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(logger)
+    )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+import games from './game'
+export default combineReducers({
+    games
+})

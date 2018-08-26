d62ba112f837f2aae5f5f9c2550d158db5bc08a5
shenfeng1945 <823902826@qq.com>
2018-08-26 21:42:34 +0800

    添加列表页

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import GameCard from './GameCard'
 const GamesList = ({games})=>{
    const emptyMessage = (
-       <p>NO Games</p>
+       <p>There are no games yet in your collection</p>
    )
    const gamesList = (
-       <p>games list</p>
+      <div className="ui four cards">
+       {games.map(game=>{
+           return <GameCard game={game} key={game._id}/>
+       })} 
+      </div>
    )
     return (
         <div>

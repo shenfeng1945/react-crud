a7aee3a8f2d61f10eae6ad2089f130a42ad6fc5f
shenfeng1945 <823902826@qq.com>
2018-08-26 10:55:15 +0800

    使用mongodb连接数据库

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongodb from 'mongodb'
 
 const app = express()
-
-app.listen('8080',()=>{console.log('server is running on localhost:8080')})
+const dbUrl = 'mongodb://localhost'
+mongodb.MongoClient.connect(dbUrl,(err,client)=>{
+    if(err) throw err;
+    const db = client.db('react-crud')
+    app.get('/api/games',(req,res)=>{
+        db.collection('games').find({}).toArray((err,games)=>{
+            res.json({games})
+        })
+    })
+    app.listen('8080',()=>{console.log('server is running on localhost:8080')})
+})

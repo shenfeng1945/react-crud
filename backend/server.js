4d8c2efddd31d254d98f52bd2c710220e4e2e7a1
shenfeng1945 <823902826@qq.com>
2018-08-31 19:54:13 +0800

    express https server not work

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import crudController from './controllers/CrudController'
+//const fs = require('fs')
+//const https = require('https')
+
+//var key = fs.readFileSync('hacksparrow-key.pem','utf8');
+//var cert = fs.readFileSync('hacksparrow-cert.pem','utf8');
+//var options = {
+//    key: key,
+//    cert: cert
+//};
 
 var app = express()
 app.use(bodyParser.json())
 crudController(app)
+//https.createServer(options,app).listen(4000)
 app.listen(4000)

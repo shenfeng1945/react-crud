adcaa616c998856544bbe8a2212b3e7ac1e595b5
shenfeng1945 <823902826@qq.com>
2018-09-18 17:42:28 +0800

    build

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,5 @@ export const ADD_GAME = 'ADD_GAME'
 export const GAME_FETCH = 'GAME_FETCH'
 export const UPDATE_GAME = 'UPDATE_GAME'
 export const GAME_DELETED = 'GAME_DELETED'
-<<<<<<< HEAD
 export const url = 'https://shenfeng1945.xyz:4000'
-=======
 export const ADD_FLASH_MSG = 'ADD_FLASH_MSG';
-export const url = ''
->>>>>>> a252d4a0de1c51ad44adfc7ab28abfb40cfb1e33
